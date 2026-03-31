b8a151eb3980f7cc09c8a54a799479ac4ce21abc
yeudisvasquez <yhvherrera@gmail.com>
2026-03-30 22:21:32 -0400

    Modify team insertion logic in insert_data.sh
    Removed ON CONFLICT clause from team insert statements.

--- insert_data.sh
+++ insert_data.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
-PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
 #clear tables
 echo $($PSQL "TRUNCATE games, teams RESTART IDENTITY CASCADE")
 #read CSV files
@@ -12,14 +20,14 @@ do
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     if [[ -z $WINNER_ID ]]
     then
-      echo $($PSQL "INSERT INTO teams(name) VALUES('$WINNER') ON CONFLICT (name) DO NOTHING;")
+      echo $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
     fi
 
     #insert opponent
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     if [[ -z $OPPONENT_ID ]]
     then
-      echo $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT') ON CONFLICT (name) DO NOTHING;")
+      echo $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
     fi
 
     #insert games
